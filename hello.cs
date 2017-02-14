c6dc69661505e333913990abc2a6caa9ce1fff51
monodrone <monodrone@users.noreply.github.com>
2017-02-14 16:24:56 -0600

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -1,13 +1,39 @@
 using System;
+using System.Text;
 
 namespace hello
 {
-   class hello
+  abstract class world
+   {
+      public abstract string word();
+   }
+   class hello:  world
+   {
+      private string first;
+      public hello(string a="hello world of Dynamic Polymorphism and Inheritance")
+      {
+         first = a;
+      }
+      public override string word ()
+      {
+         Console.WriteLine("The word is :");
+         return (first);
+      }
+   }
+
+   class outputworld
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello world");
+         hello h = new hello();
+         string nh = h.word();
+         byte[] hexy = Encoding.Default.GetBytes(nh);
+         var hexyinStrings = BitConverter.ToString(hexy);
+         hexyinStrings = hexyinStrings.Replace("-", "");
+
+         Console.WriteLine(nh);
+         Console.WriteLine("Hex of string: \n" + hexyinStrings);
          Console.ReadKey();
       }
-   }
-}
+    }
+  }
